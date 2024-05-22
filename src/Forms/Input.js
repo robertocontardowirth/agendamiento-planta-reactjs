9bf7f6efc21bfb2838fab99d104efe3960d26d06
Roberto Contardo <robertocontardowirth@gmail.com>
2024-05-22 16:11:08 -0400

    Avance validación paso 1

--- src/Forms/Input.js
+++ src/Forms/Input.js
@@ -1,5 +1,6 @@
 import { forwardRef } from "react";
 
 export const Input = forwardRef((props, ref) => {
+  //console.log(props, ref)
   return <input ref={ref} className="form-control" {...props} />;
 });
--- src/Steps/Inicio.js
+++ src/Steps/Inicio.js
@@ -3,19 +3,24 @@ import { useNavigate } from "react-router-dom";
 import { useAppState } from "../State";
 import { Button, Field, Form, Input } from "../Forms";
 import FeatherIcon from 'feather-icons-react';
+import { validate, format } from 'rut.js';
  
 export const Inicio = () => {
   const [state, setState] = useAppState();
   const {
     handleSubmit,
     register,
-    //watch,
-    formState: { errors },
-  } = useForm({ defaultValues: state, mode: "onSubmit" });
+    watch,
+    formState: { isDirty, isValid, errors },
+	setValue
+  } = useForm({ defaultValues: state, mode: "onChange" });
   //const watchPassword = watch("password");
   const navigate = useNavigate();
+
+  console.log(errors)
  
   const saveData = (data) => {
+	console.log(data)
     setState({ ...state, ...data });
     navigate("/residuo");
   };
@@ -25,42 +30,106 @@ export const Inicio = () => {
         <h2>Inicio</h2>
         <Field label="RUT Cliente" error={errors?.rutCliente}>
           <Input
-            {...register("rutCliente", { required: "El RUT de Cliente es requerido" })}
+            {...register("rutCliente", {
+				required: {
+					value: true,
+					message: "El RUT de Cliente es requerido"
+				},
+				validate: (value) => {
+					return validate(value) || "RUT inválido"
+				},
+				onChange: (event) => {
+					const formattedRut = format(event.target.value)
+					setValue('rutCliente', formattedRut)
+				}
+			})}
+			minLength="9"
+			maxLength="12"
             id="rut-cliente"
           />
         </Field>
         <Field label="Correo de Notificación" error={errors?.email}>
           <Input
-            {...register("email", { required: "El correo de notificación es requerido" })}
+            {...register("email", { 
+				required: "El correo de notificación es requerido",
+				pattern: {
+					value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+					message: 'Correo inválido',
+				},
+			})}
             type="email"
             id="email"
           />
         </Field>
         <Field label="RUT Transportista" error={errors?.rutTransportista}>
           <Input
-            {...register("rutTransportista", { required: "El RUT de Transportista es requerido" })}
+            {...register("rutTransportista", { 
+				required: {
+					value: true,
+					message: "El RUT de Transportista es requerido"
+				},
+				validate: (value) => {
+					return validate(value) || "RUT inválido"
+				},
+				onChange: (event) => {
+					const formattedRut = format(event.target.value)
+					setValue('rutTransportista', formattedRut)
+				}
+			})}
+			minLength="9"
+			maxLength="12"
             id="rut-transportista"
           />
         </Field>
         <Field label="Patente" error={errors?.patente}>
           <Input
-            {...register("patente", { required: "La patente del vehículo es requerido" })}
-            id="rut-transportista"
+            {...register("patente", { 
+				required: "La patente del vehículo es requerido" 
+			})}
+            id="patente"
+          />
+        </Field>
+		<Field label="RUT Generador" error={errors?.rutGenerador}>
+          <Input
+            {...register("rutGenerador", { 
+				required: {
+					value: true,
+					message: "El RUT de Generador es requerido"
+				},
+				validate: (value) => {
+					return validate(value) || "RUT inválido"
+				},
+				onChange: (event) => {
+					const formattedRut = format(event.target.value)
+					setValue('rutGenerador', formattedRut)
+				}
+			})}
+			minLength="9"
+			maxLength="12"
+            id="rut-generador"
           />
         </Field>
         <Field label="Comuna" error={errors?.comuna}>
           <Input
-            {...register("comuna", { required: "La comuna es requerida" })}
+            {...register("comuna", { 
+				required: "La comuna es requerida" 
+			})}
             id="comuna"
           />
         </Field>
         <Field label="Dirección" error={errors?.direccion}>
           <Input
-            {...register("direccion", { required: "La direccion es requerida" })}
+            {...register("direccion", { 
+				required: "La direccion es requerida" 
+			})}
             id="direccion"
           />
         </Field>
-        <Button variant="next">Siguiente <FeatherIcon icon="chevron-right" size="18" /></Button>
+        <Button variant="next" disabled={!isDirty || !isValid}>Siguiente <FeatherIcon icon="chevron-right" size="18" /></Button>
+        <pre>
+			{JSON.stringify(watch(), null, 2)}
+		</pre>
+        
     </Form>
   );
 };
