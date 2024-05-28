8028fe89c5ffee5388b24716db5eef766def4786
Roberto Contardo <robertocontardowirth@gmail.com>
2024-05-28 02:01:47 -0400

    Avance paso 3

--- src/Steps/Fecha.js
+++ src/Steps/Fecha.js
@@ -4,6 +4,7 @@ import { useAppState } from "../State";
 import { Button, Field, Form } from "../Forms";
 import FeatherIcon from 'feather-icons-react';
 import DatePicker from "react-datepicker";
+import Select from 'react-select'
 
 import "react-datepicker/dist/react-datepicker.css";
  
@@ -16,6 +17,12 @@ export const Fecha = () => {
     setState({ ...state, ...data });
     navigate("/confirmacion");
   };
+
+  const horario = [
+    { value: "07:00", label: "07:00" },
+    { value: "08:00", label: "08:00" },
+    { value: "09:00", label: "09:00" }
+  ]
  
   return (
     <Form onSubmit={handleSubmit(saveData)}>
@@ -31,6 +38,22 @@ export const Fecha = () => {
                   selected={field.value}
                 />
             )}
+          />
+        </Field>
+        <Field label="Hora" error={errors?.comuna}>
+          <Controller
+            name="hora"
+            rules={{ 
+              required: "Hora de llegada" 
+            }}
+            control={control}
+            render={({ field: { onChange }, value, ref }) => <Select
+                inputRef={ref}
+                classNamePrefix="form-select"
+                options={horario}
+                value={horario.find(c => c.value === value)}
+                onChange={val => onChange(val.value)}
+            />}
           />
         </Field>
         <div className="button-row">
